d75eaa32f32957481ff492d47cc6f96e0be29918
Matt Jenkins <matt@majenko.co.uk>
2025-03-05 23:55:55 +0000

    Added hex2bin to complement oct2bin

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: novadis oct2bin
+all: novadis oct2bin hex2bin
 
 novadis: novadis.o
 	$(CC) -o $@ $^
 
 oct2bin: oct2bin.o
+	$(CC) -o $@ $^
+
+hex2bin: hex2bin.o
 	$(CC) -o $@ $^
---
+++ hex2bin.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		fprintf(stderr, "Usage: hex2bin <infile> <outfile>\n");
+		return -1;
+	}
+
+	if (access(argv[1], R_OK) != 0) {
+		fprintf(stderr, "Error: cannot access %s\n", argv[1]);
+		return -1;
+	}
+	FILE *in = fopen(argv[1], "r");
+	int out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
+	char buf[1024];
+	while (fgets(buf, 1024, in)) {
+		uint16_t val = strtoul(buf, NULL, 16);
+		write(out, &val, 2);
+	}
+	fclose(in);
+	close(out);
+}
